94600a9eec3974b160492718e22cfb9f45b9f10c
MyfengNull <ysr200808@163.com>
2016-07-29 15:44:32 +0800

    增加链接

--- mybl/src/main/java/com/ysr/mybl/MainActivity.java
+++ mybl/src/main/java/com/ysr/mybl/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        init();
+
         initView();
     }
 
@@ -52,7 +52,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 // set the adapter when printing by way of Bluetooth
 //                }
                 getBluetoothAdapter();
-                print();
+
                 break;
             case R.id.btN2:
                 Intent intent = new Intent(MainActivity.this, SearchActivity.class);
@@ -77,87 +77,9 @@ public class MainActivity extends Activity implements View.OnClickListener {
         return bluetoothAdapter;
     }
 
-    public void print() {
-        Thread trd = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                String externalStorageDir = Environment.getExternalStorageDirectory().toString();
-                // define printer and printer setting information
-                Printer printer = new Printer();
-                PrinterInfo printInfo = new PrinterInfo();
-                printInfo.printerModel = PrinterInfo.Model.RJ_3150;
-                printInfo.port = PrinterInfo.Port.BLUETOOTH;
-                printInfo.customPaper = externalStorageDir + "/rj3150_76mm.bin";
-                printInfo.macAddress = "00:11:EE:BB:AA:CC";
-                printer.setPrinterInfo(printInfo);
-                // Pass Bluetooth adapter to the library (Bluetooth only)
-                BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-                printer.setBluetooth(bluetoothAdapter);
-                //print
-                String srcPath = externalStorageDir + "/sample.png";
-                Log.e("srcPath", srcPath);
-                PrinterStatus status = printer.printFile(srcPath);
-            }
-        });
-        trd.start();
-    }
 
-    /**
-     * copy from raw in resource
-     */
-    private void raw2file(String fileName, int fileID) throws Exception {
-        File newdir = new File(Common.CUSTOM_PAPER_FOLDER);
-        if (!newdir.exists()) {
-            newdir.mkdir();
-        }
-        File dstFile = new File(Common.CUSTOM_PAPER_FOLDER + fileName);
-        if (!dstFile.exists()) {
-            try {
-                InputStream input = null;
-                OutputStream output = null;
-                input = this.getResources().openRawResource(fileID);
-                output = new FileOutputStream(dstFile);
-                int DEFAULT_BUFFER_SIZE = 1024 * 4;
-                byte[] buffer = new byte[DEFAULT_BUFFER_SIZE];
-                int n = 0;
-                while (-1 != (n = input.read(buffer))) {
-                    output.write(buffer, 0, n);
-                }
-                input.close();
-                output.close();
-            } catch (FileNotFoundException e1) {
-            } catch (IOException e) {
-            }
-        }
-    }
 
-    private void init() {
-         /*
-         * copy .bin file (RJ paper size info.) to
-		 * /mnt/sdcard/customPaperFileSet/ .bin file is made by Printer Setting
-		 * Tool which can be downloaded from the Brother Net Site
-		 */
-        try {
-            raw2file("RJ4030_102mm.bin", R.raw.rj4030_102mm);
-            raw2file("RJ4030_102mm152mm.bin", R.raw.rj4030_102mm152mm);
-            raw2file("RJ4040_102mm.bin", R.raw.rj4040_102mm);
-            raw2file("RJ4040_102mm152mm.bin", R.raw.rj4040_102mm152mm);
-            raw2file("RJ4030Ai_102mm.bin", R.raw.rj4030ai_102mm);
-            raw2file("RJ4030Ai_102mm152mm.bin", R.raw.rj4030ai_102mm152mm);
-            raw2file("RJ3050_76mm.bin", R.raw.rj3050_76mm);
-            raw2file("RJ3150_76mm.bin", R.raw.rj3150_76mm);
-            raw2file("RJ3150_76mm44mm.bin", R.raw.rj3150_76mm44mm);
-            raw2file("TD2020_57mm.bin", R.raw.td2020_57mm);
-            raw2file("TD2020_40mm40mm.bin", R.raw.td2020_40mm40mm);
-            raw2file("TD2120_57mm.bin", R.raw.td2120_57mm);
-            raw2file("TD2120_40mm40mm.bin", R.raw.td2120_40mm40mm);
-            raw2file("TD2130_57mm.bin", R.raw.td2130_57mm);
-            raw2file("TD2130_40mm40mm.bin", R.raw.td2130_40mm40mm);
-            raw2file("TD4100N_102mm.bin", R.raw.td4100n_102mm);
-            raw2file("TD4100N_102mm152mm.bin", R.raw.td4100n_102mmx152mm);
-            raw2file("TD4000_102mm.bin", R.raw.td4000_102mm);
-            raw2file("TD4000_102mm152mm.bin", R.raw.td4000_102mmx152mm);
-        } catch (Exception e) {
-        }
-    }
+
+
+
 }
--- mybl/src/main/java/com/ysr/mybl/SearchActivity.java
+++ mybl/src/main/java/com/ysr/mybl/SearchActivity.java
@@ -1,26 +1,25 @@
 package com.ysr.mybl;
 
-import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
-import android.view.Window;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import com.lvrenyang.io.BTPrinting;
+import com.lvrenyang.io.IOCallBack;
+import com.lvrenyang.io.Pos;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -28,7 +27,7 @@ import java.util.concurrent.Executors;
 /*
 2.0蓝牙
  */
-public class SearchActivity extends AppCompatActivity implements View.OnClickListener {
+public class SearchActivity extends AppCompatActivity implements View.OnClickListener, IOCallBack {
 
     private Button btnSearch;
     private LinearLayout linearLayoutdevices;
@@ -37,6 +36,10 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
     private ProgressBar pb;
     static SearchActivity mActivity;
     private final int MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 00011;
+    //蓝牙匹配
+    private ExecutorService es = Executors.newScheduledThreadPool(30);
+    Pos mPos = new Pos();
+    BTPrinting mBt = new BTPrinting();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,8 +51,10 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
 
         btnSearch = (Button) findViewById(R.id.btnSearch);
         btnSearch.setOnClickListener(this);
+
+        mPos.Set(mBt);
+        mBt.SetCallBack(this);
         initBroadcast();
- 
     }
 
 
@@ -60,20 +65,16 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
                 String action = intent.getAction();
                 BluetoothDevice device = intent
                         .getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-
                 if (BluetoothDevice.ACTION_FOUND.equals(action)) {
                     Toast.makeText(mActivity, "ACTION_FOUND...", Toast.LENGTH_SHORT).show();
                     if (device == null) {
                         return;
                     }
-
                     final String address = device.getAddress();
                     String name = device.getName();
-
                     if (name == null)
                         name = "BT";
                     else if (name.equals(address)) {
-
                         name = "BT";
                     }
                     Button button = new Button(context);
@@ -90,13 +91,16 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
                         @Override
                         public void onClick(View view) {
                             for (int i = 0; i < linearLayoutdevices.getChildCount(); ++i) {
+                                //搜索按钮禁用
+                                btnSearch.setEnabled(false);
                                 Button btn = (Button) linearLayoutdevices.getChildAt(i);
                                 btn.setEnabled(false);
                             }
-
+                            es.submit(new TaskOpen(mBt, address));
                         }
                     });
                     button.getBackground().setAlpha(100);
+                    //显示
                     linearLayoutdevices.addView(button);
                 } else if (BluetoothAdapter.ACTION_DISCOVERY_STARTED
                         .equals(action)) {
@@ -157,4 +161,67 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
             grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
+
+    //回调
+    @Override
+    public void OnOpen() {
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < linearLayoutdevices.getChildCount(); i++) {
+                    Button btn = (Button) linearLayoutdevices.getChildAt(i);
+                    btn.setEnabled(false);
+                }
+                Toast.makeText(mActivity, "Connected", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void OnOpenFailed() {
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                linearLayoutdevices.setEnabled(true);
+                for (int i = 0; i < linearLayoutdevices.getChildCount(); i++) {
+                    Button btn = (Button) linearLayoutdevices.getChildAt(i);
+                    btn.setEnabled(true);
+                }
+                Toast.makeText(mActivity, "Failed", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void OnClose() {
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                linearLayoutdevices.setEnabled(true);
+                for (int i = 0; i < linearLayoutdevices.getChildCount(); i++) {
+                    Button btn = (Button) linearLayoutdevices.getChildAt(i);
+                    btn.setEnabled(true);
+                }
+                Toast.makeText(mActivity, "Close", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+    public class TaskOpen implements Runnable {
+        BTPrinting bt = null;
+        String address = null;
+
+        public TaskOpen(BTPrinting bt, String address) {
+            this.bt = bt;
+            this.address = address;
+        }
+
+        @Override
+        public void run() {
+            // TODO Auto-generated method stub
+            bt.Open(address);
+        }
+    }
+
 }
